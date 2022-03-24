ef1a68bc48784c68c64efd50ddcb113cb9298e5e
Georgi Georgiev <georgi.dimitrov@limechain.tech>
2022-03-24 13:52:27 +0200

    Update REAME.md and hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,20 +10,17 @@ import "solidity-coverage";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {
+      forking: {
+        url: process.env.MAINNET_FORK_URL as string,
+      },
     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
